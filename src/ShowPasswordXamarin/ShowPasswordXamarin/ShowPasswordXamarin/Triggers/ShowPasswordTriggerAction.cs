783a3114afcbcc303ad640b86234c131c0ea4ec5
Wilson Vargas <wilsonvargas_6@outlook.com>
2019-08-14 19:11:41 -0500

    Initial commit

---
+++ src/ShowPasswordXamarin/ShowPasswordXamarin/ShowPasswordXamarin/Triggers/ShowPasswordTriggerAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace ShowPasswordXamarin.Triggers
+{
+    public class ShowPasswordTriggerAction : TriggerAction<Button>
+    {
+        public string IconImageName { get; set; }
+
+        public string EntryPasswordName { get; set; }
+
+        protected override void Invoke(Button sender)
+        {
+            Image imageIconView = ((Grid)sender.Parent).FindByName<Image>(IconImageName);
+            Entry entryPasswordView = ((Grid)((Grid)sender.Parent).Parent).FindByName<Entry>(EntryPasswordName);
+            string imageFile = string.Empty;
+
+            if (entryPasswordView.IsPassword)
+            {
+                imageFile = (Device.RuntimePlatform == Device.UWP) ? "Assets/hideicon.png" : "hideicon.png";               
+            }
+            else
+            {
+                imageFile = (Device.RuntimePlatform == Device.UWP) ? "Assets/showicon.png" : "showicon.png";
+            }
+            imageIconView.Source = ImageSource.FromFile(imageFile);
+            entryPasswordView.IsPassword = !entryPasswordView.IsPassword;
+            entryPasswordView.CursorPosition = (entryPasswordView.Text != null) ? entryPasswordView.Text.Length : 0;            
+        }
+    }
+}
